c81dc68ac7e439cbcf4897bfc0af959bbfcdcb5d
unknown <C:\Users\7-2-16\AppData\Roaming\The Bat!>
2019-03-01 17:57:16 +0500

    Работал с менюшкой

--- js/menu.js
+++ js/menu.js
@@ -3,8 +3,12 @@ $(document).ready(function() {
 	// бургер
 	$('.burger').click(function(e){
 		e.preventDefault();
-		$('.menu').toggleClass('active');
-		$('.btn-close').toggleClass('active');
+		//$('.menu').toggleClass('active');
+		$('.menu').removeClass('close').addClass('active');
+	});
+
+	$('.btn-close').click(function(e){
+		$('.menu').removeClass('active').addClass('close');
 	});
 
 });
