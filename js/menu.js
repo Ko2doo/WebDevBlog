1ffcb93ad53e63dc427e5f26633b5c75e5bddb41
Ko2doo <Ko2doo@github.com>
2019-03-03 03:07:02 +0500

    Бургер меню готово 100%

--- js/menu.js
+++ js/menu.js
@@ -3,12 +3,21 @@ $(document).ready(function() {
 	// бургер
 	$('.burger').click(function(e){
 		e.preventDefault();
-		//$('.menu').toggleClass('active');
 		$('.menu').removeClass('close').addClass('active');
 	});
 
 	$('.btn-close').click(function(e){
 		$('.menu').removeClass('active').addClass('close');
+
+			//Добавим удаление класса 'close' через определенное время,
+			//тем самым полностью скрыв объект т.к. класс '.menu' имеет свойство display:none;
+			setTimeout(function(e){
+				$('.menu').removeClass('close');
+			}, 1000);
 	});
+
+
+
+
 
 });
